6bb234712f239d0705baf73a5f38be1b36c8ebff
robteaw <robteaw@gmail.com>
2021-05-20 20:58:16 -0500

    3. Service Fix

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import camera from "../img/camera.png";
 import photoEdit from "../img/photo-edit.jpg";
