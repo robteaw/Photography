3f4783a754bfe771258dd9b972538985cbfd8614
robteaw <robteaw@gmail.com>
2021-05-30 12:23:49 -0500

    9. Minor Fixes

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 // Styling and Animation
-import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { motion } from "framer-motion";
 import { popup } from "../animations";
 
 export default function ContactSection() {
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import styled from "styled-components";
 import heroImg from "../images/heroImg.jpg";
 // Styling and Animation
